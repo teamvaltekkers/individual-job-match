697122b7169a684434a951d4b1a6b02bcdadf5e2
alangorton <alan.gorton@valtech.co.uk>
2017-11-24 11:53:35 +0000

    adds quiz questions

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,14 +2,34 @@ import React, { Component } from 'react';
 import TechnicalQuiz from './TechnicalQuiz';
 
 class Quiz extends Component {
-    componentWillMount() {
-        this.state = TechnicalQuiz;
+    constructor() {
+        super();
+
+        this.state = {
+            quiz: TechnicalQuiz,
+            questionIndex: 0
+        };
     }
 
     render() {
+        const index = this.state.questionIndex;
+        const question = this.state.quiz[index];
+        const answers = question.answers.map(answer => {
+            return (
+                <div key={answer.index}>
+                    {answer.index} - {answer.answer}
+                </div>
+            );
+        });
+
         return (
             <div>
-                Quiz
+                <div>
+                    {question.question}
+                </div>
+                <div>
+                    {answers}
+                </div>
             </div>
         );
     }
