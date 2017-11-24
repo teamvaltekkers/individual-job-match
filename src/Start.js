3ff2f2c762541d6b27b2a18fecde9aa5ee00b8bd
Gareth.Joyce <gareth.joyce@GarethJoyces-MacBook-Pro.local>
2017-11-24 13:49:45 +0000

    adding start page content.

--- src/Start.js
+++ src/Start.js
@@ -8,9 +8,11 @@ class Start extends Component {
     render() {
         return (
             <div>
-                <header>
-                    <h1 >Welcome to Individual Job Match</h1>
-                </header>
+                <h1 class="heading-large">Welcome to Individual Talent Match</h1>
+                <p class="lead">The Individual Talent Match is a measure used by employers to show how much of a logic thinker you are. Your results will show whether you have a technical brain and recruiters looking for technical roles will be matched to you.</p>
+                <p>The Talent Match is multiple choice.</p>
+                <p>Click the start button to begin.</p>
+
                 <Link to="/quiz/1" />
             </div>
         );
