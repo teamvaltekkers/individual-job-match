a9334a6c9b817e5a0733dd3b82e3fcd92c2954b1
alangorton <alan.gorton@valtech.co.uk>
2017-11-24 15:04:11 +0000

    adds second job

--- src/JobSwipe.js
+++ src/JobSwipe.js
@@ -9,7 +9,7 @@ class JobSwipe extends Component {
         };
     }
 
-    render() {
+    job1 = () => {
         return (
             <div className="card">
                 <div className="card__header">
@@ -22,6 +22,30 @@ class JobSwipe extends Component {
                     <p>Their team is growing, so our client is on the lookout for a talented Technical Engineer to join their Operations Team, based at their Aldershot office.</p>
                 </div>
             </div>
+        );
+    }
+
+    job2 = () => {
+        return (
+            <div className="card">
+                <div className="card__header">
+                    <h3 className="job-title">Technical Engineer 2</h3>
+                    <span className="job-location">Aldershot, Hampshire</span>
+                    <span className="job-salary">Up to £40,000 (Dependent on Experience) + Extensive Benefits Package </span>
+                </div>
+                <div className="card__body">
+                    <p>Our client is a global provider of technology systems and solutions across the entire insurance industry. They provide core technology solutions, distribution and trading capability, advanced analytics and solution delivery.</p>
+                    <p>Their team is growing, so our client is on the lookout for a talented Technical Engineer to join their Operations Team, based at their Aldershot office.</p>
+                </div>
+            </div>
+        );
+    }
+
+    render() {
+        const job = this.job1();
+        
+        return (
+            {job}
         );
     }
 }
